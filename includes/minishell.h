d74f876493589445f9ae79f6bdeb15c76cf6de33
Gonzalo Aguado Torres <gonagutor@gmail.com>
2022-02-02 19:28:35 +0100

    Needed draft

---
+++ includes/minishell.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguado- <gaguado-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 18:54:34 by gaguado-          #+#    #+#             */
+/*   Updated: 2022/02/02 19:26:09 by gaguado-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define RESET				"\x1b[0m"
+# define WHITE				"\x1b[1m"
+# define W_B_WHITE			"\x1b[47m"
+# define W_B_CYAN			"\x1b[46m"
+# define W_B_PURPLE			"\x1b[45m"
+# define W_B_BLUE			"\x1b[44m"
+# define W_B_YELLOW			"\x1b[43m"
+# define W_B_GREEN			"\x1b[42m"
+# define W_B_RED			"\x1b[41m"
+# define WH_B_CLEAR			"\x1b[40m"
+# define INVISIBLE			"\x1b[8m"
+# define WHITE_BOX			"\x1b[7m"
+# define WHITE_SUB			"\x1b[4m"
+# define WHITE_CURS			"\x1b[3m"
+# define GRAY				"\x1b[2m"
+# define BLACK				"\x1b[30m"
+# define RED				"\x1b[31m"
+# define GREEN				"\x1b[32m"
+# define YELLOW				"\x1b[33m"
+# define BLUE				"\x1b[34m"
+# define PURPLE				"\x1b[35m"
+# define CYAN				"\x1b[36m"
+
+#endif
